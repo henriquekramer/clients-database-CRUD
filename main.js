7eb503369bc0e6b62550451dfd73a143f99e9fba
Henrique <henriquemkramer@gmail.com>
2022-02-24 13:33:51 -0300

    CRUD JS update

--- main.js
+++ main.js
@@ -1,9 +1,43 @@
+'use strict'
 
 const openModal = () => document.getElementById('modal').classList.add('active')
 
 const closeModal = () => document.getElementById('modal').classList.remove('active')
 
+const tempClient = {
+  nome: "Carlos",
+  email: "carlos@gmail.com",
+  celular: "1199999-4545",
+  cidade: "São Roque"
+}
 
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? []
+const setLocalStorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient))
+
+// CRUD- create read update delete
+
+const deleteClient = (index) => {
+  const dbClient = readClient()
+  dbClient.splice(index, 1)
+  setLocalStorage(dbClient)
+}
+
+const updateClient = (index, client) => {
+  const dbClient = readClient()
+  dbClient[index] = client
+  setLocalStorage(dbClient)
+}
+
+const readClient = () => getLocalStorage()
+
+const createClient = (client) => {
+  const dbClient = getLocalStorage()
+  dbClient.push (client)
+  setLocalStorage(dbClient)
+}
+
+
+// Eventos
 document.getElementById('cadastrarCliente').addEventListener('click', openModal)
 
 document.getElementById('modalClose').addEventListener('click', closeModal)
